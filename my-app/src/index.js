f902963d4c0b670e354acfc3a569d3b7f28d0dc8
zhucaiyun <zhucaiyun1@xdf.cn>
2022-04-26 16:13:59 +0800

    feat:提交game

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,27 +2,48 @@
  * @Author       : zhucaiyun1@xdf.cn
  * @Date         : 2022-04-25 15:43:48
  * @LastEditors  : zhucaiyun1@xdf.cn
- * @LastEditTime : 2022-04-25 18:25:17
+ * @LastEditTime : 2022-04-26 16:05:33
  * @Description  : 描述信息
  */
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import './index.css'
 
-class Square extends React.Component { 
-  render() { 
-    return (
-      <button className="square"></button>
-    )
-  }
-}
+// class Square extends React.Component { 
+//   render() { 
+//     return (
+//       <button className="square" onClick={() =>this.props.onClick()} >{ this.props.value }</button>
+//     )
+//   }
+// }
 
+// 函数组件
+function Square(props) { 
+  return (
+    <button className="square" onClick={props.onClick}>{ props.value }</button>
+  )
+}
 class Board extends React.Component { 
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+      isWinter: null
+    }
+  }
+
+  handleClick(i) { 
+    const squares = this.state.squares.slice()
+    squares[i] = this.state.xIsNext? 'X':'O'
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext ,isWinter: calculateWinner(squares)})
+  }
+
   renderSquare(i) { 
-    return <Square />;
+    return <Square value={this.state.squares[i]} onClick={() => { this.handleClick(i)}}/>;
   }
   render() { 
-    const status = 'Next player: X'
+    const status = 'Next player: ' + (this.state.xIsNext?'X':'O') + 'winner:'+this.state.isWinter
     return(
       <div>
         <div className="status"> {status} </div>
@@ -60,7 +81,30 @@ class Game extends React.Component {
     )
   }
 }
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-)
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<Game />)
+
+
+// 判断胜出者
+function calculateWinner(squares) { 
+  console.log(squares)
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) { 
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) { 
+      console.log(squares[a])
+      return squares[a]
+    }
+  }
+  return null
+}
